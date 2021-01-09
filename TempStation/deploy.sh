f0aa607df6e407b0666e58214eb39792a80d5cb4
Atanas Bozhanin <atanas.bozhanin@gmail.com>
2021-01-09 22:47:06 +0200

    Update deploy.sh script.

--- TempStation/deploy.sh
+++ TempStation/deploy.sh
@@ -1,2 +1,5 @@
 dotnet publish -c Release
 sudo cp -R bin/Release/net5.0/publish/* /var/www/TempStation/
+sudo chown www-data:www-data -R /var/www/TempStation/
+sudo cp /var/www/appsettings.json /var/www/TempStation/
+sudo journalctl -f
