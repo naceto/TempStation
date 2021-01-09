65389ca59dca7d7775b135f779a1ea12cdd67823
Atanas Bozhanin <atanas.bozhanin@gmail.com>
2021-01-09 18:56:55 +0200

    Deploy script copies the release appsettings.json

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 dotnet publish -c Release
 sudo cp -R bin/Release/net5.0/publish/* /var/www/TempStation/
+sudo cp /var/www/appsettings.json /var/www/TempStation/
 sudo chown www-data:www-data -R /var/www/TempStation
