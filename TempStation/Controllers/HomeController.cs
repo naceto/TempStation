213b0a0d5a4b65d5c8c6ab378b30ebdfe9592cc7
Atanas Bozhanin <atanas.bozhanin@gmail.com>
2021-02-14 14:51:13 +0200

    Adding Authorize attribute.

--- TempStation/Controllers/HomeController.cs
+++ TempStation/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
@@ -116,6 +117,7 @@ namespace TempStation.Controllers
             return View(chartViewData);
         }
 
+        [Authorize]
         public async Task<IActionResult> MySensors() 
         {
 
